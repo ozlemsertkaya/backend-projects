ecc2ec72aed7c1fe9ce1ea2a7c68a443d7e0287b
Özlem Sertkaya <ozlemsrtkyy0@gmail.com>
2026-08-28 22:02:20 +0300

    Yorum satırları eklendi.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    // 
+    //$fillable:Listedeki yazan alanları doldurur ve kaydeder.Yani kullanıcıdan gelen her şeyi veritabanına kaydedebilirdik.Bunu kötüye kullanmayı engellemiş olduk.
     protected $fillable = ['name', 'description', 'price', 'stock'];
+    //protected:sınıf özelliklerine erişimi belirler.
 }
--- database/migrations/2026_08_28_074527_create_products_table.php
+++ database/migrations/2026_08_28_074527_create_products_table.php
@@ -10,14 +10,14 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
+    {   //Schema veritabanında işlem yapmamızı sağlar.
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->text('description')->nullable();
-            $table->decimal('price', 10, 2);
-            $table->integer('stock')->default(0);
-            $table->timestamps();
+            $table->id(); //id nin otomatik arttığı bir sütun oluşturtuk.
+            $table->string('name'); //Ürün adını yazabileceğimiz kısa metin sütunu " "
+            $table->text('description')->nullable(); //Ürün için açıklama yapacağımız uzun metin sütunu " ".Buradaki nullable boş bırakabileceğimizi gösterir.
+            $table->decimal('price', 10, 2); //price adında ondalık sütun " ".
+            $table->integer('stock')->default(0); //stok durumunu takip edeceğimiz sütun.Tam sayı olduğu için(integer).Eğer değer yoksa 0 olur.(default(0))
+            $table->timestamps(); //created_at ve updated_at olmak üzere iki sütun ekler ki kaydın yapıldığını ve ne zmn güncellendiğini tutsun.
         });
     }
 
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('products'); //Burada silme işlemi yapıyoruz.up fonksiyonu kurmuştu migration mimarisini.Migrate rollback çalıştırıldığında down fonk. devreye girer.Tabloyu siler.(yanlış bir şey yaptığımızda sütunlarda bunu kullanırız.)
     }
 };
